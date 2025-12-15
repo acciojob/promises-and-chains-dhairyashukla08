6b854ccbaba4145c3b2419695fd4e8af31c8c345
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-15 16:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,32 +3,25 @@ const nameInput = document.getElementById('name');
 const ageInput = document.getElementById('age');
 const submitButton = document.getElementById('btn');
 
-
 function checkEligibility(age, name) {
-
   return new Promise((resolve, reject) => {
-
     setTimeout(() => {
-    
       if (age > 18) {
-
         resolve(`Welcome, ${name}. You can vote.`);
       } else {
-        
         reject(`Oh sorry ${name}. You aren't old enough.`);
       }
     }, 4000);
   });
 }
-form.addEventListener('submit', function(event) {
 
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const nameValue = nameInput.value.trim();
   const ageValue = parseInt(ageInput.value, 10);
 
   if (nameValue === '' || isNaN(ageValue) || ageValue === null || ageValue < 0) {
-   
     alert("Please enter valid details.");
     return;
   }
@@ -37,14 +30,13 @@ form.addEventListener('submit', function(event) {
   submitButton.textContent = 'Processing...';
 
   checkEligibility(ageValue, nameValue)
-    .then((successMessage) => 
+    .then((successMessage) => {
       alert(successMessage);
     })
     .catch((errorMessage) => {
       alert(errorMessage);
     })
     .finally(() => {
-
       submitButton.disabled = false;
       submitButton.textContent = 'Check Eligibility';
     });
